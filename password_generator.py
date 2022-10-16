d7962d79b9d63f4f4b4627ba54d9472d8f1c0581
Maciej Woźniewicz <maciej.wozniewicz01@gmail.com>
2022-10-16 15:20:06 +0200

    commit

---
+++ password_generator.py
@@ -0,0 +1,22 @@
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + string.punctuation)
+
+def generate_password():
+    password_length = int(input("How long would you like your password to be? "))
+
+    random.shuffle(characters)
+
+    password = []
+
+    for x in range(password_length):
+        password.append(random.choice(characters))
+
+    random.shuffle(password)
+
+    password = "".join(password)
+
+    return password
+
+print(generate_password())
